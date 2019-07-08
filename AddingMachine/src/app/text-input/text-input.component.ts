1063d26e4225bc74808834ffbf17f1194c19e6d4
Caleb Powell <calebpowell@zipcoders-MacBook-Pro-2.local>
2019-07-08 12:54:03 -0400

    made work

--- AddingMachine/src/app/text-input/text-input.component.ts
+++ AddingMachine/src/app/text-input/text-input.component.ts
@@ -7,7 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextInputComponent implements OnInit {
 
-  constructor() { }
+  log: any = "123123";
+
+  value = '';
+  onEnter(value: string) { this.log = this.run(value); }
+  ops: string[] = [];
+  vals: number[] = [];
+
+  constructor() {}
+
+  public run(toEvaluate: string): number{
+    let tokens: string[] = toEvaluate.split(" ");
+    for(let token of tokens){
+      if(token === "("){}
+        else if (token === "+")    this.ops.push(token);
+        else if (token === "-")    this.ops.push(token);
+        else if (token === "*")    this.ops.push(token);
+        else if (token === "/")    this.ops.push(token);
+        else if (token === "sqrt") this.ops.push(token);
+        else if (token === ")") {
+          let op: string = this.ops.pop();
+          let v: number = this.vals.pop();
+          if      (op === "+")    v = this.vals.pop() + v;
+          else if (op === "-")    v = this.vals.pop() - v;
+          else if (op === "*")    v = this.vals.pop() * v;
+          else if (op === "/")    v = this.vals.pop() / v;
+          else if (op === "sqrt") v = Math.sqrt(v);
+          this.vals.push(v);
+        }
+        else this.vals.push(Number.parseFloat(token));
+    }
+    return this.vals.pop();
+  }
+
+  public returnsAString(): string{
+    return "hello world";
+  }
 
   ngOnInit() {
   }
